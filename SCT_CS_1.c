3f1488314a69ed31041eb9e7922b779986df2ff1
kundanasiri13 <siirrii13@gmail.com>
2025-09-07 12:03:04 +0530

    Update code

---
+++ SCT_CS_1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+// Function to encrypt text
+void caesarEncrypt(char text[], int shift) {
+    for (int i = 0; text[i] != '\0'; i++) {
+        char ch = text[i];
+        
+        if (isalpha(ch)) {
+            char base = isupper(ch) ? 'A' : 'a';
+            text[i] = (ch - base + shift) % 26 + base;
+        }
+    }
+}
